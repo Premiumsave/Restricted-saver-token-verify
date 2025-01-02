2e2f65287b5f8793ec70cbf6d475d18f0b592978
Premiumsave <ynapxdzxecmclyalc12345678@gmail.com>
2025-01-02 19:36:10 +0530

    Update shrink.py

--- devgagan/modules/shrink.py
+++ devgagan/modules/shrink.py
@@ -68,7 +68,7 @@ async def token_handler(client, message):
                 "Hi 👋 Welcome, Wanna intro...?\n\n"
                 "✳️ I can save posts from channels or groups where forwarding is off. I can download videos/audio from YT, INSTA, ... social platforms\n"
                 "✳️ Simply send the post link of a public channel. For private channels, do /login. Send /help to know more. \n\n"
-                "> 👉 **Made By Spark Developer (Owner)** "
+                "> 👉 **Made By Yusuf Developer (Owner)** "
             ),
             reply_markup=keyboard
         )
