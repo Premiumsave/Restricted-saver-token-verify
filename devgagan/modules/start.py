c47afab9abccbfdc2add44bb7f7c6506b8194d30
Premiumsave <ynapxdzxecmclyalc12345678@gmail.com>
2025-01-02 19:38:37 +0530

    Update start.py

--- devgagan/modules/start.py
+++ devgagan/modules/start.py
@@ -175,7 +175,7 @@ async def terms(client, message):
 @app.on_message(filters.command("plan") & filters.private)
 async def plan(client, message):
     plan_text = (
-        "💰 **Premium Price**: Starting from ₹15 per day accepted via **Upi** to purchase contact @Personal123premium (terms and conditions apply).\n"
+        "💰 **Premium Price**: Starting from ₹15 per day and 100rs for 1 months 😇accepted via **Upi** to purchase contact @Personal123premium (terms and conditions apply).\n"
         "📥 **Download Limit**: Users can download up to 10,00,000 files in a single batch command.\n"
         "🛑 **Batch**: You will get two modes /bulk and /batch.\n"
         "   - Users are advised to wait for the process to automatically cancel before proceeding with any downloads or uploads.\n\n"
@@ -194,7 +194,7 @@ async def plan(client, message):
 @app.on_callback_query(filters.regex("see_plan"))
 async def see_plan(client, callback_query):
     plan_text = (
-       "💰 **Premium Price**: Starting from ₹15 per day accepted via **Upi** to purchase contact @Personal123premium (terms and conditions apply).\n"
+       "💰 **Premium Price**: Starting from ₹15 per day and 100rs for 1 months accepted via **Upi** to purchase contact @Personal123premium (terms and conditions apply).\n"
         "📥 **Download Limit**: Users can download up to 10,00,000 files in a single batch command.\n"
         "🛑 **Batch**: You will get two modes /bulk and /batch.\n"
         "   - Users are advised to wait for the process to automatically cancel before proceeding with any downloads or uploads.\n\n"
