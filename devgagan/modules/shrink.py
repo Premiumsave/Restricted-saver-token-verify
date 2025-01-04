2236976313dbc897045abf8d4a9fcfa26358e13c
Premiumsave <ynapxdzxecmclyalc12345678@gmail.com>
2025-01-05 00:08:19 +0530

    Update shrink.py

--- devgagan/modules/shrink.py
+++ devgagan/modules/shrink.py
@@ -58,7 +58,7 @@ async def token_handler(client, message):
     if len(message.command) <= 1:
         image_url = "https://files.catbox.moe/lwd4k1.jpg"
         keyboard = InlineKeyboardMarkup(
-            [[InlineKeyboardButton("Developer", url="https://t.me/Traderpersonaladvice2314")],
+            [[InlineKeyboardButton("Join Channel", url="https://t.me/Traderpersonaladvice2314")],
              [InlineKeyboardButton("Buy Premium", url="https://t.me/Traderpersonaladvice2314")]])
 
         # Send the message with the image and keyboard
