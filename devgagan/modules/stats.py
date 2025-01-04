bf023ce2039c33cc51bd4b14c019145f61d78d53
Premiumsave <ynapxdzxecmclyalc12345678@gmail.com>
2025-01-04 23:51:41 +0530

    Update stats.py

--- devgagan/modules/stats.py
+++ devgagan/modules/stats.py
@@ -30,6 +30,6 @@ async def stats(client, message):
 **Total Users** : {users}
 **Premium Users** : {len(premium)}
 
-**__Powered by @Personal123premium__**
+**__Powered by @Traderpersonaladvice2314__**
 """)
   
